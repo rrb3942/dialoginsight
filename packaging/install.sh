8c805cbcd28b5d326c30e4d1e265b86e029d9938
Ryan Bullock <ryan@piratel.com>
2023-03-25 21:50:29 -0700

    Provide updates during install script

--- packaging/install.sh
+++ packaging/install.sh
@@ -9,20 +9,22 @@ DOC="usr/share/doc/${PROJECT}"
 MAN="usr/share/man/man1"
 INIT="usr/lib/systemd/system"
 
+echo "Making directories"
 mkdir -p "${PREFIX}${BIN}"
 mkdir -p "${PREFIX}${ETC}"
 mkdir -p "${PREFIX}${DOC}"
 mkdir -p "${PREFIX}${MAN}"
 mkdir -p "${PREFIX}${INIT}"
 
+echo "Copying files"
 cp -f ${BIN}/* "${PREFIX}${BIN}"
 cp -f ${DOC}/* "${PREFIX}${DOC}"
 cp -f ${INIT}/* "${PREFIX}${INIT}"
 cp -f ${MAN}/* "${PREFIX}${MAN}"
 cp -n ${ETC}/* "${PREFIX}${ETC}"
 
+echo "Setting permissions"
 for FILE in ${BIN}/*; do
-	echo "${PREFIX}${BIN}/${FILE##*/}"
 	chown root:root "${PREFIX}${BIN}/${FILE##*/}"
 done
 
@@ -37,5 +39,11 @@ done
 chown root:root -R "${PREFIX}${ETC}"
 chown root:root -R "${PREFIX}${DOC}"
 
+echo "Adding system user"
+set +e
 adduser --system dialoginsight --shell /sbin/nologin &> /dev/null
+
+echo "Refreshing daemon list"
 systemctl daemon-reload
+
+echo "dialoginsight installed"
